22080b601a3289a79147749acf9f8a588e46eade
LisaTr <trlis369@gmail.com>
2020-11-01 10:40:31 -0500

    FallMood and Rain are added

---
+++ src/main/java/FallMood.java
@@ -0,0 +1,6 @@
+public class FallMood {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Rain.java
@@ -0,0 +1,6 @@
+public class Rain {
+
+    public static void main(String[] args) {
+        System.out.println("November the 1st Rainy day");
+    }
+}

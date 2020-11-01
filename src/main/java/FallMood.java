cb839113da67f01567000cc4961f55f85a807e3d
LisaTr <trlis369@gmail.com>
2020-11-01 11:24:39 -0500

    Staying at home added

--- src/main/java/FallMood.java
+++ src/main/java/FallMood.java
@@ -4,6 +4,7 @@ public class FallMood {
         System.out.println("Today is raining whole  and it feels like fall");
         //Fall mood is here
         //It's cold today
+        //I like this kind of weather if I stay home
 
     }
 }

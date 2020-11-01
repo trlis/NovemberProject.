64a036fcdb30e339b523d19ff6859fe7a0e08cfb
LisaTr <trlis369@gmail.com>
2020-11-01 10:56:23 -0500

    Weather today

--- src/main/java/FallMood.java
+++ src/main/java/FallMood.java
@@ -1,6 +1,8 @@
 public class FallMood {
 
     public static void main(String[] args) {
+        System.out.println("Today is raining whole  and it feels like fall");
+        //Fall mood is here
 
     }
 }
